8fc6ada01b626e867886ffd25f958817a30e4e27
Yuta <jp.yuta0131@gmail.com>
2025-02-15 13:56:24 +0900

    update

--- .config/nvim/lua/plugins/nvim-notify.lua
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -1,6 +1,11 @@
 return {
-	"rcarriga/nvim-notify",
-	config = function()
-		vim.notify = require("notify")
-	end,
+  "rcarriga/nvim-notify",
+  config = function()
+    require("notify").setup({
+      render = "compact",
+      stages = "fade",
+      max_width = 80,
+    })
+    vim.notify = require("notify")
+  end,
 }
--- .zshrc
+++ .zshrc
@@ -3,3 +3,10 @@
 for script in $HOME/.config/zsh/*.zsh; do
   [ -r "$script" ] && source "$script"
 done
+
+# bun completions
+[ -s "/home/yuta/.bun/_bun" ] && source "/home/yuta/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
