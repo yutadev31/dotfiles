81c0ef9fd597d66bdace861f4634b7beb911e3e7
Yuta <jp.yuta0131@gmail.com>
2025-02-28 01:41:48 +0900

    update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,4 +6,11 @@ vim.api.nvim_create_autocmd("TermOpen", {
   end,
 })
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
+})
+
 vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" }) -- 外部変更の自動検出
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,7 +18,29 @@ set("t", "<ESC>", "<C-\\><C-n>")
 
 -- telescope
 local builtin = require("telescope.builtin")
-set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+set("n", "<leader>ff", builtin.find_files, { desc = "ファイル検索" })
+set("n", "<leader>fg", builtin.live_grep, { desc = "単語検索" })
+set("n", "<leader>fb", builtin.buffers, { desc = "開いているバッファ一覧" })
+set("n", "<leader>fh", builtin.help_tags, { desc = "ヘルプ検索" })
+
+-- LSP関連
+set("n", "<leader>fr", builtin.lsp_references, { desc = "シンボルの参照一覧" })
+set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "ファイル内のシンボル検索" })
+set("n", "<leader>fw", builtin.lsp_workspace_symbols, { desc = "ワークスペース全体のシンボル検索" })
+
+-- Git関連 (Gitプラグインが必要)
+set("n", "<leader>gc", builtin.git_commits, { desc = "Gitコミット履歴" })
+set("n", "<leader>gb", builtin.git_branches, { desc = "Gitブランチ一覧" })
+set("n", "<leader>gs", builtin.git_status, { desc = "Gitの変更ファイル" })
+
+local opts = { noremap = true, silent = true }
+
+-- LSP基本操作
+set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)          -- 定義へジャンプ
+set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)         -- 宣言へジャンプ
+set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)          -- 参照を検索
+set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)      -- 実装へジャンプ
+set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)                -- ドキュメント表示
+set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)      -- 変数リネーム
+set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- コードアクション
