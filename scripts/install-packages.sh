793896cb2a03c6a38f04db6b42b5eec1651d4ae0
Yuta <jp.yuta0131@gmail.com>
2025-02-28 12:08:41 +0900

    add: scripts/install-packages.sh

---
+++ scripts/install-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pacman -Syu \
+  base base-devel \
+  zip unzip \
+  curl wget \
+  git github-cli \
+  zsh tmux \
+  vim neovim \
+  festfetch \
+  man-db man-pages \
+  nodejs npm yarn pnpm \
+  cmake nasm rustup \
+  openssh translate-shell tree zip unzip 
