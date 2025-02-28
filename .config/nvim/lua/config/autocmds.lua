fed65c64fa293c60cbdcbfaa4b3e7a5e3f4b4d0b
Yuta <jp.yuta0131@gmail.com>
2025-02-28 12:15:57 +0900

    update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
-    vim.lsp.buf.format({ async = true })
+    vim.lsp.buf.format({ async = false })
   end,
 })
 
