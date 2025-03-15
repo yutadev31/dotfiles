be47f2d9d299190ff731fbcf91faabef423a22fa
Yuta <jp.yuta0131@gmai.com>
2025-03-16 04:42:17 +0900

    update

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo pacman -Syu --needed \
-  base base-devel \
-  zip unzip \
-  curl wget \
-  git github-cli \
-  zsh tmux starship \
-  vim neovim helix \
-  fastfetch \
-  man-db man-pages \
-  nodejs npm yarn pnpm \
-  cmake nasm rustup \
-  openssh translate-shell tree zip unzip
+	base base-devel \
+	zip unzip \
+	curl wget \
+	git github-cli \
+	zsh tmux starship \
+	vim neovim helix \
+	fastfetch \
+	man-db man-pages \
+	nodejs npm yarn pnpm \
+	cmake nasm rustup \
+	openssh translate-shell tree zip unzip
