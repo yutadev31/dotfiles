b5dd85923fad744feaa795b807ede75317350e2f
Yuta <jp.yuta0131@gmail.com>
2025-03-30 09:47:58 +0900

    config: Update

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,12 +6,54 @@ return {
   "echasnovski/mini.nvim",
   version = "*",
   config = function()
-    setup("icons", {})
-    setup("comment", {})
+    -- Text editing --
+    setup("ai", {})
+    setup("align", {})
+    -- setup("comment", {})
+    -- setup("completion", {})
+    setup("move", {})
+    setup("operators", {})
     setup("pairs", {})
+    setup("snippets", {})
+    setup("splitjoin", {})
     setup("surround", {})
-    setup("hipatterns", {})
-    setup("starter", {})
+
+    -- General workflow --
+    setup("basics", {})
+    setup("bracketed", {})
+    setup("bufremove", {})
+    setup("clue", {})
+    -- setup("deps", {})
+    -- setup("diff", {})
+    setup("extra", {})
+    setup("files", {})
+    setup("git", {})
+    setup("jump", {})
+    setup("jump2d", {})
+    setup("misc", {})
+    setup("pick", {})
     setup("sessions", {})
+    setup("visits", {})
+
+    -- Appearance --
+    -- setup("animate", {})
+    -- setup("base16", {})
+    setup("colors", {})
+    setup("cursorword", {})
+    setup("hipatterns", {})
+    -- setup("hues", {})
+    setup("icons", {})
+    -- setup("indentscope", {})
+    setup("map", {})
+    -- setup("notify", {})
+    setup("starter", {})
+    -- setup("statusline", {})
+    setup("tabline", {})
+    setup("trailspace", {})
+
+    -- Other --
+    setup("doc", {})
+    setup("fuzzy", {})
+    setup("test", {})
   end,
 }
