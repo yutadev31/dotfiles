9cae91cd59db7abbf4642bea69690f33a0431c74
Yuta <jp.yuta0131@gmail.com>
2025-04-11 08:01:57 +0900

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,22 +1,20 @@
 local h = require("utils.helper")
-local opts = { noremap = true, silent = true }
 
 -- ▼ ターミナルモードのESCキーでノーマルモードへ
 h.tmap("<ESC>", "<C-\\><C-n>")
 
 h.imap("<c-space>", function()
-  vim.lsp.completion.get()
+	vim.lsp.completion.get()
 end)
 
 h.nmap("<leader>ee", "<CMD>Oil<CR>")
 h.nmap("<leader>tt", "<CMD>ToggleTerm<CR>")
 h.nmap("<leader>mm", "<CMD>Mason<CR>")
 
--- ▼ LSP基本操作
-h.nmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)          -- 定義へジャンプ
-h.nmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)         -- 宣言へジャンプ
-h.nmap("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)          -- 参照を検索
-h.nmap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)      -- 実装へジャンプ
-h.nmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)                -- ドキュメント表示
-h.nmap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)      -- 変数リネーム
-h.nmap("<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- コードアクション
+h.nmap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+h.nmap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+h.nmap("gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+h.nmap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+h.nmap("K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+h.nmap("<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+h.nmap("<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -8,7 +8,7 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.prettier,
+        -- null_ls.builtins.formatting.prettier,
       },
     })
   end,
