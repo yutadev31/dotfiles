0f01687f810f7273733baa1ff41fa33fbf9f1f5f
Yuta <jp.yuta0131@gmail.com>
2025-04-18 10:40:35 +0900

    fix: 空のバッファがバッファラインに表示される問題を修正

--- .config/nvim/lua/ui/window.lua
+++ .config/nvim/lua/ui/window.lua
@@ -6,6 +6,12 @@ function M.open_sidebar(title, width)
   vim.api.nvim_buf_set_name(buf, title)
 
   vim.cmd("topleft vnew")
+  local tmp_buf = vim.api.nvim_get_current_buf()
+  vim.bo[tmp_buf].buftype = "nofile"
+  vim.bo[tmp_buf].buflisted = false
+  vim.bo[tmp_buf].swapfile = false
+  vim.bo[tmp_buf].bufhidden = "wipe"
+
   local win = vim.api.nvim_get_current_win()
   vim.api.nvim_win_set_buf(win, buf)
   vim.api.nvim_win_set_width(win, width)
