fe4304046def0f41cccc520d99e7044a872ef57d
Yuta <jp.yuta0131@gmail.com>
2025-04-22 17:39:45 +0900

    refactor::

--- .config/nvim/lua/yutadev31/config/options.lua
+++ .config/nvim/lua/yutadev31/config/options.lua
@@ -1,6 +1,6 @@
 local h = require("yutadev31.utils.helper")
 
--- ▸ Basic Settings
+-- Basic Settings
 vim.opt.encoding = "utf-8"
 vim.opt.title = true
 vim.opt.termguicolors = true
@@ -9,7 +9,7 @@ vim.opt.timeout = true
 vim.opt.timeoutlen = 1000
 vim.opt.shell = "/usr/bin/fish"
 
--- ▸ UI / Display
+-- UI / Display
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
@@ -22,7 +22,7 @@ vim.opt.linebreak = true
 vim.opt.whichwrap:append("<,>,h,l")
 vim.opt.fillchars:append({ eob = "~" })
 
--- ▸ Indentation
+-- Indentation
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -30,47 +30,47 @@ vim.opt.softtabstop = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
--- ▸ Search Behavior
+-- Search Behavior
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
 
--- ▸ Clipboard Integration
+-- Clipboard Integration
 vim.opt.clipboard:append("unnamedplus")
 
--- ▸ Whitespace Characters
+-- Whitespace Characters
 vim.opt.list = true
 vim.opt.listchars = { tab = "> ", trail = "-", nbsp = "+" }
 
--- ▸ Backspace Behavior
+-- Backspace Behavior
 vim.opt.backspace = "indent,eol,start"
 
--- ▸ Completion & Performance
+-- Completion & Performance
 vim.opt.shortmess:append("c")
 vim.opt.updatetime = 300
 
--- ▸ Disable Swap Files
+-- Disable Swap Files
 vim.opt.swapfile = false
 
--- ▸ Auto Reload on File Change
+-- Auto Reload on File Change
 vim.opt.autoread = true
 
--- ▸ Window Split Behavior
+-- Window Split Behavior
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.showmode = false
 vim.opt.cmdheight = 0
 
--- ▸ Folding (with Treesitter)
+-- Folding (with Treesitter)
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.foldenable = true
 
--- ▸ Colorscheme
+-- Colorscheme
 vim.cmd("colorscheme shadow-hue")
 
--- ▸ Filetype-specific Tab Settings
+-- Filetype-specific Tab Settings
 h.filetype({ "go", "make" }, function()
   h.tabsize(false, 4)
 end)
