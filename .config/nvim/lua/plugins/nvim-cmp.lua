b18bfd5f2de112c77126524c99f32803a93ee65b
Yuta <jp.yuta0131@gmail.com>
2025-04-23 02:13:15 +0900

    chore: remove unused plugins
    Removed unnecessary plugins from the Neovim configuration to clean up the setup.

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -6,13 +6,8 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/vim-vsnip",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
-    "echasnovski/mini.snippets",
-    "abeldekat/cmp-mini-snippets",
 
     "onsails/lspkind.nvim",
   },
@@ -47,8 +42,6 @@ return {
         { name = "spell" },
         { name = "buffer" },
         { name = "path" },
-        { name = "cmdline" },
-        { name = "vsnip" },
         { name = "luasnip" },
       }),
     })
