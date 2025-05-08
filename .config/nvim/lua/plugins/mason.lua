73377339867b2a1beed92d26df9ad150b54b8a31
Yuta <jp.yuta0131@gmail.com>
2025-05-08 12:48:07 +0900

    fix: 仮修正

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -20,42 +20,42 @@ return {
         automatic_installation = true,
       })
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-                workspace = {
-                  library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
-                  },
-                },
-                format = {
-                  enable = false,
-                },
-              },
-            },
-          })
-        end,
-        ["rust_analyzer"] = function()
-          require("lspconfig").rust_analyzer.setup({
-            settings = {
-              ["rust-analyzer"] = {
-                diagnostic = {
-                  refreshSupport = false,
-                },
-              },
-            },
-          })
-        end,
-      })
+      -- require("mason-lspconfig").setup_handlers({
+      --   function(server_name)
+      --     require("lspconfig")[server_name].setup({})
+      --   end,
+      --   ["lua_ls"] = function()
+      --     require("lspconfig").lua_ls.setup({
+      --       settings = {
+      --         Lua = {
+      --           diagnostics = {
+      --             globals = { "vim" },
+      --           },
+      --           workspace = {
+      --             library = {
+      --               [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+      --               [vim.fn.stdpath("config") .. "/lua"] = true,
+      --             },
+      --           },
+      --           format = {
+      --             enable = false,
+      --           },
+      --         },
+      --       },
+      --     })
+      --   end,
+      --   ["rust_analyzer"] = function()
+      --     require("lspconfig").rust_analyzer.setup({
+      --       settings = {
+      --         ["rust-analyzer"] = {
+      --           diagnostic = {
+      --             refreshSupport = false,
+      --           },
+      --         },
+      --       },
+      --     })
+      --   end,
+      -- })
     end,
   },
 }
