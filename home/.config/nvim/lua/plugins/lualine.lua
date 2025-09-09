c5a324f8c2bc246b4c7016596da63e7d5f042952
Yuta <jp.yuta0131@gmail.com>
2025-09-10 04:30:55 +0900

    change: テーマを自作の物に変更

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -6,6 +6,7 @@ return {
   },
   opts = {
     options = {
+      theme = "yoru",
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       globalstatus = true,
