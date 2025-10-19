0bec90ebb368d2fe519c5a7772aa7f109ace2ea8
Yuta <yuta256dev@gmail.com>
2025-10-19 12:40:01 +0900

    update

--- installer/install.sh
+++ installer/install.sh
@@ -48,6 +48,7 @@ boot_part_dev="$(add_partition $disk_dev 1)"
 root_part_dev="$(add_partition $disk_dev 2)"
 
 ucode="$(get_ucode)"
+shell="fish"
 
 p1=$(read_password "Enter password: ")
 echo
@@ -95,82 +96,18 @@ pacstrap -K /mnt $(cat packages/base.txt)
 log "Generating fstab..."
 genfstab -U /mnt >>/mnt/etc/fstab
 
-log "Creating post-install script..."
-cat <<EOF >/mnt/post-install.sh
-set -euo pipefail
-
-GREEN='\033[1;32m'
-RESET='\033[0m'
-
-log() {
-  echo -e "\${GREEN}[*] \$1\${RESET}"
-}
-
-log "Initializing pacman keys..."
-pacman-key --init
-pacman-key --populate archlinux
-
-log "Enabling colors in pacman..."
-sed -i 's/^#\(Color\)/\1/' /etc/pacman.conf
-
-log "Download package databases..."
-pacman -Syu
-
-log "Configuring timezone and clock..."
-timedatectl set-timezone $time_zone
-hwclock --systohc
-
-log "Setting locale and keyboard layout..."
-echo "LANG=$lang" >/etc/locale.conf
-echo "KEYMAP=$keymap" >/etc/vconsole.conf
-sed -i 's/^#\(en_US\.UTF-8 UTF-8\)/\1/' /etc/locale.gen
-locale-gen
-
-log "Setting hostname..."
-echo "$hostname" >/etc/hostname
-
-log "Installing GRUB bootloader..."
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-log "Enabling essential services..."
-systemctl enable NetworkManager sshd
-
-log "Configuring /etc/hosts..."
-cat <<END >/etc/hosts
-127.0.0.1   localhost
-::1         localhost
-127.0.1.1   $hostname.localdomain $hostname
-END
-
-log "Configuring NetworkManager to use Cloudflare DNS..."
-mkdir -p /etc/NetworkManager/conf.d
-cat <<END >/etc/NetworkManager/conf.d/dns.conf
-[main]
-dns=none
-END
-
-log "Setting Cloudflare DNS..."
-cat <<END >/etc/resolv.conf
-nameserver 1.1.1.2
-nameserver 1.0.0.2
-END
-
-log "Configuring sudo permissions..."
-sed -i 's/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
-visudo -c
-
-log "Creating user '$username'..."
-useradd -m -G wheel -s /bin/$shell $username
-
-log "Generating initramfs..."
-mkinitcpio -P
-
-rm /post-install.sh
-EOF
+log "Copying post-install script..."
+cp post-install.sh /mnt/post-install.sh
 
 log "Running chroot setup..."
-arch-chroot /mnt /bin/bash /post-install.sh
+arch-chroot /mnt /bin/env \
+  username="$username" \
+  time_zone="$time_zone" \
+  lang="$lang" \
+  keymap="$keymap" \
+  hostname="$hostname" \
+  shell="$shell" \
+  /post-install.sh
 
 log "Setting root and user passwords..."
 echo "$password" | arch-chroot /mnt /bin/passwd --stdin
