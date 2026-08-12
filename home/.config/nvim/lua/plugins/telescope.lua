6650ede02c72eae65b215915e847ce08302452b2
Yuta <yuta256dev@gmail.com>
2026-08-12 17:23:08 +0900

    update

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,6 @@ return {
   tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   event = "VeryLazy",
   config = function()
