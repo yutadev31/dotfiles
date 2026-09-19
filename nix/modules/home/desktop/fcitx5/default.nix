c6efbb0fc2b5415fa6953bc55bcf2f5948a77d9f
Yuta <yuta256dev@gmail.com>
2026-09-19 18:42:05 +0900

    refactor(fcitx5): generate input method groups from list

--- nix/modules/home/desktop/fcitx5/default.nix
+++ nix/modules/home/desktop/fcitx5/default.nix
@@ -1,4 +1,23 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  groups = [
+    {
+      name = "Japanese";
+      layout = "jp";
+      im = "mozc";
+    }
+    {
+      name = "Korean";
+      layout = "kr";
+      im = "hangul";
+    }
+    {
+      name = "Chinese";
+      layout = "cn";
+      im = "pinyin";
+    }
+  ];
+in
 {
   i18n.inputMethod = {
     enable = true;
@@ -18,43 +37,35 @@
           "EnumerateGroupBackwardKeys/0" = "Shift+Muhenkan";
         };
       };
-      inputMethod = {
-        GroupOrder."0" = "Japanese";
-        GroupOrder."1" = "Korean";
-        GroupOrder."2" = "Chinese";
-
-        # Japanese
-        "Groups/0" = {
-          Name = "Japanese";
-          "Default Layout" = "jp";
-          DefaultIM = "mozc";
-        };
-        "Groups/0/Items/0".Name = "keyboard-jp";
-        "Groups/0/Items/1".Name = "mozc";
-
-        # Korean
-        "Groups/1" = {
-          Name = "Korean";
-          "Default Layout" = "kr";
-          DefaultIM = "hangul";
-        };
-        "Groups/1/Items/0".Name = "keyboard-kr";
-        "Groups/1/Items/1".Name = "hangul";
-
-        # Chinese
-        "Groups/2" = {
-          Name = "Chinese";
-          "Default Layout" = "cn";
-          DefaultIM = "pinyin";
-        };
-        "Groups/2/Items/0".Name = "keyboard-cn";
-        "Groups/2/Items/1".Name = "pinyin";
-      };
       addons = {
         classicui.globalSection = {
           Theme = "Tokyonight-Storm";
         };
       };
+      inputMethod = lib.mkMerge (
+        lib.imap0 (
+          i: group:
+          let
+            id = toString i;
+          in
+          {
+            GroupOrder.${id} = group.name;
+
+            "Groups/${id}" = {
+              Name = group.name;
+              "Default Layout" = group.layout;
+            }
+            // lib.optionalAttrs (group ? im) {
+              DefaultIM = group.im;
+            };
+
+            "Groups/${id}/Items/0".Name = "keyboard-${group.layout}";
+          }
+          // lib.optionalAttrs (group ? im) {
+            "Groups/${id}/Items/1".Name = group.im;
+          }
+        ) groups
+      );
     };
   };
 }
