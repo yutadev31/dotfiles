aece8562f8ac7408b0c76fc8be8c7b7b96871202
Yuta <yuta256dev@gmail.com>
2026-09-26 10:19:27 +0900

    refactor(nvim): replace blink.cmp with nvim-cmp

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,19 +1,36 @@
 return {
-  "saghen/blink.cmp",
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
   dependencies = {
-    "saghen/blink.lib",
-    "rafamadriz/friendly-snippets",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
   },
-  build = function()
-    require("blink.cmp").build():pwait()
-  end,
+  config = function()
+    local cmp = require("cmp")
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = "default" },
-    -- completion = { documentation = { auto_show = false } },
-    sources = { default = { "lsp", "path", "snippets", "buffer" } },
-    -- fuzzy = { implementation = "rust" },
-  },
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          vim.snippet.expand(args.body)
+        end,
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+      }),
+
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "path" },
+      }, {
+        { name = "buffer" },
+      }),
+    })
+  end,
 }
