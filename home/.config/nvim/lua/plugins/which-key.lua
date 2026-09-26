e76dc38fa4f22c3b37543fc2b7698b3607299bdd
Yuta <yuta256dev@gmail.com>
2026-09-26 14:21:35 +0900

    feat(nvim): add which-key plugin

---
+++ home/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    spec = {
+      { "<leader>f", group = "find" },
+      { "<leader>g", group = "git" },
+      { "<leader>x", group = "trouble" },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
