d9800227e9e8a110d3059036b3ba7cebd6c36704
Yuta <yuta256dev@gmail.com>
2026-09-26 14:25:28 +0900

    refactor(nvim): move Trouble keymaps to plugin config

--- home/.config/nvim/lua/plugins/trouble.lua
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -2,4 +2,10 @@ return {
   "folke/trouble.nvim",
   opts = {},
   cmd = "Trouble",
+  keys = {
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
+    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
+    { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo" },
+  },
 }
