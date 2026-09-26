806d61a4aa123c7c6f036f29aa4bcdc6c39fcc3c
Yuta <yuta256dev@gmail.com>
2026-09-26 14:23:52 +0900

    feat(nvim): add neogit plugin

---
+++ home/.config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,19 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = { "sindrets/diffview.nvim" },
+  opts = {
+    disable_hint = false,
+    disable_commit_confirmation = true,
+    kind = "split",
+    integrations = { diffview = true },
+    sections = {
+      recent = { folded = false },
+      untracked = { folded = false },
+      unstaged = { folded = false },
+      staged = { folded = false },
+    },
+  },
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
+}
